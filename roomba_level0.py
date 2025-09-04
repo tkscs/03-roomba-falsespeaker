2f8332692a20722da61e9cd375ffb0843c61fa33
GITHUB_USERNAME <KEHILLAH_EMAIL>
2025-09-04 09:22:20 -0700

    roomba moves

--- roomba_level0.py
+++ roomba_level0.py
@@ -10,12 +10,22 @@ import room
 
 # Draw the Level 0 version of the room
 window = room.draw_room(level = 0)
-
+fd=forward
+rt=right
+lt=left
 ###
 # Start your code here
- 
- 
- 
+for i in range(2):
+    fd(160)
+    lt(90)
+    fd(40)
+    lt(90)
+    fd(160)
+    rt(90)
+    fd(40)
+    rt(90)
+fd(160)
+
 # End your code here
 ###
  
